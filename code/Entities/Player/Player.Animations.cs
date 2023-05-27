20419292e4a7ab3f9a2c7f9c1c78de4108de6cf7
yuberee <59583743+yuberee@users.noreply.github.com>
2023-05-27 12:01:51 +0200

    Better player animation and controller

---
+++ code/Entities/Player/Player.Animations.cs
@@ -0,0 +1,33 @@
+﻿namespace BombSurvival;
+
+public partial class Player
+{
+	public CitizenAnimationHelper Animations => new CitizenAnimationHelper( this );
+
+	Vector3 currentLookAt = Vector3.Zero;
+	Vector3 nextLookAt = Vector3.Zero;
+
+	public void ComputeAnimations()
+	{
+
+		var animationHelper = new CitizenAnimationHelper( this );
+
+		if ( InputRotation == new Rotation() )
+			if ( !Velocity.IsNearlyZero( 1 ) )
+				nextLookAt = Velocity.Normal;
+			else
+			 nextLookAt = Vector3.Zero;
+		else
+			nextLookAt = InputRotation.Forward;
+
+		currentLookAt = Vector3.Lerp( currentLookAt, nextLookAt + Vector3.Right, Time.Delta * 5f );
+
+		var wishRotation = Rotation.LookAt( currentLookAt.WithZ( 0f ), Vector3.Up );
+		Rotation = Rotation.Lerp( Rotation, wishRotation, Time.Delta * 10f );
+
+		animationHelper.WithLookAt( Position + currentLookAt );
+
+		animationHelper.WithVelocity( Velocity );
+		animationHelper.IsGrounded = GroundEntity != null;
+	}
+}
---
+++ code/Entities/Player/Player.Controller.cs
@@ -0,0 +1,80 @@
+﻿namespace BombSurvival;
+
+public partial class Player
+{
+	public float WalkSpeed => 120f;
+	public float RunSpeed => 200f;
+	public float AccelerationSpeed => 600f; // Units per second (Ex. 200f means that after 1 second you've reached 200f speed)
+	public float WishSpeed { get; private set; } = 0f;
+	public Vector3 Direction { get; set; } = Vector3.Zero;
+
+	public Vector3 WishVelocity => Direction.Normal * WishSpeed;
+	public Rotation WishRotation => Rotation.LookAt( Direction, Vector3.Up );
+	public float StepSize => 8f;
+	public float MaxWalkableAngle => 55f;
+
+
+	public TimeSince TimeSinceLostFooting = 0f;
+
+	public void ComputeMotion()
+	{
+		Direction = InputDirection.RotateAround( Vector3.Up, Rotation.FromYaw( 90f ) ).WithY( 0f );
+
+		if ( Direction != Vector3.Zero )
+			WishSpeed = Math.Clamp( WishSpeed + AccelerationSpeed * Time.Delta, 0f, Input.Down( "run" ) ? RunSpeed : WalkSpeed );
+		else
+			WishSpeed = 0f;
+
+		Velocity = Vector3.Lerp( Velocity, WishVelocity, 15f * Time.Delta ) // Smooth horizontal movement
+			.WithZ( Velocity.z ); // Don't smooth vertical movement
+
+		if ( TimeSinceLostFooting > Time.Delta * 5f )
+			Velocity -= Vector3.Down * (TimeSinceLostFooting + 1f) * Game.PhysicsWorld.Gravity * Time.Delta;
+
+		if ( Input.Down( "jump" ) )
+		{
+			if ( GroundEntity != null )
+			{
+				GroundEntity = null;
+				Velocity = Velocity.WithZ( 300f );
+				Animations.TriggerJump();
+			}
+
+			if ( Velocity.z > 0f ) // Floaty jump
+				Velocity += Vector3.Up * -Game.PhysicsWorld.Gravity * Time.Delta / 2f;
+		}
+
+		var helper = new MoveHelper( Position, Velocity );
+		helper.MaxStandableAngle = MaxWalkableAngle;
+
+		helper.Trace = helper.Trace
+			.Size( CollisionBox.Mins, CollisionBox.Maxs )
+			.Ignore( this );
+
+		if ( GroundEntity == null )
+			helper.TryMove( Time.Delta );
+		else
+			helper.TryMoveWithStep( Time.Delta, StepSize );
+
+
+		Position = helper.Position;
+		Velocity = helper.Velocity;
+
+		var traceDown = helper.TraceDirection( Vector3.Down * 5f );
+
+		if ( traceDown.Entity != null )
+		{
+			GroundEntity = traceDown.Entity;
+			Position = traceDown.EndPosition;
+
+			if ( Vector3.GetAngle( Vector3.Up, traceDown.Normal ) <= helper.MaxStandableAngle )
+				TimeSinceLostFooting = 0f;
+		}
+		else
+		{
+			GroundEntity = null;
+			TimeSinceLostFooting = 0f;
+			Velocity += Vector3.Down * -Game.PhysicsWorld.Gravity * Time.Delta;
+		}
+	}
+}
