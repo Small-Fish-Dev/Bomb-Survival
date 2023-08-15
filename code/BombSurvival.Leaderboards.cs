260b3ffba0e2c35fd608006a7046d725ffb9b98f
yuberee <59583743+yuberee@users.noreply.github.com>
2023-08-15 17:21:49 +0200

    Leaderboard WIP, ignore!

---
+++ code/BombSurvival.Leaderboards.cs
@@ -0,0 +1,25 @@
+﻿namespace BombSurvival;
+
+public partial class BombSurvival
+{
+	/*
+	[ClientRpc]
+	internal static void SendScore( IClient client, int value )
+	{
+		if ( !Game.IsEditor )
+		{
+			if ( !client.IsValid() || client.IsBot ) return;
+
+			Sandbox.Services.Stats.SetValue( client, $"{CurrentLevel} Highscores" , value );
+			Log.Info( "Score sent" );
+		}
+	}
+
+	[ConCmd.Admin]
+	public static void TestScore( int value )
+	{
+		var caller = ConsoleSystem.Caller;
+
+		SendScore( To.Single( caller ), caller, value );
+	}*/
+}
