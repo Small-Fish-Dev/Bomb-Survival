7afbb97d7195082466f7eb6fb369428c7afd68be
yuberee <59583743+yuberee@users.noreply.github.com>
2023-08-18 15:41:45 +0200

    Entity.Glow extension

--- code/Utilities/Extensions/Entity.Extensions.cs
+++ code/Utilities/Extensions/Entity.Extensions.cs
@@ -1,4 +1,6 @@
-﻿namespace BombSurvival;
+﻿using Sandbox.Component;
+
+namespace BombSurvival;
 
 public static class EntityExtensions
 {
@@ -8,5 +10,33 @@ public static class EntityExtensions
 		if ( entity is Player player ) return player;
 		if ( entity.Owner is Player owner ) return owner;
 		return null;
+	}
+
+	public static void Glow( this Entity entity, bool on, Color color )
+	{
+		if ( !on )
+		{
+			if ( entity.Components.TryGet<Glow>( out Glow oldGlow ) )
+				oldGlow.Enabled = false;
+
+			foreach ( var child in entity.Children )
+			{
+				if ( child.Components.TryGet<Glow>( out Glow childOldGlow ) )
+					childOldGlow.Enabled = false;
+			}
+		}
+		else
+		{
+			var newGlow = entity.Components.GetOrCreate<Glow>();
+			newGlow.Enabled = true;
+			newGlow.Color = color;
+
+			foreach ( var child in entity.Children )
+			{
+				var newChildGlow = child.Components.GetOrCreate<Glow>();
+				newChildGlow.Enabled = true;
+				newChildGlow.Color = color;
+			}
+		}
 	}
 }
