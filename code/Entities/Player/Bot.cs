71953613c21ea6087846abc07dbf9d8ca43afd7f
yuberee <59583743+yuberee@users.noreply.github.com>
2023-08-19 22:35:52 +0200

    Custom bots initial

---
+++ code/Entities/Player/Bot.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox.Utility;
+
+namespace BombSurvival;
+
+public class BombSurvivalBot : Bot
+{
+	[ConCmd.Admin( "bs_bot_add" )]
+	internal static void SpawnCustomBot()
+	{
+		Sandbox.Game.AssertServer();
+		new BombSurvivalBot();
+	}
+
+	private float randSeed;
+
+	public BombSurvivalBot()
+	{
+	}
+
+	public override void BuildInput()
+	{
+		if ( Client.Pawn is not Player player ) return;
+
+		Input.SetAction( "Jump", true );
+		//Input.AnalogMove = pawn.Rotation.Forward;
+
+		//TODO Set InputRotation
+	}
+
+	public override void Tick()
+	{
+		if ( Client.Pawn is not Player player ) return;
+	}
+}
