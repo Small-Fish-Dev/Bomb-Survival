339050a45e724e8efc46b635088e3c906ecead3c
yuberee <59583743+yuberee@users.noreply.github.com>
2023-08-20 15:01:42 +0200

    Generate grid with jump definitions

--- code/BombSurvival.Grid.cs
+++ code/BombSurvival.Grid.cs
@@ -7,22 +7,23 @@ public partial class BombSurvival
 {
 	public static float WorldWidth => 2048f;
 	public static float WorldHeight => 2048f;
-	public static BBox WorldBox => new BBox( new Vector3( -WorldWidth / 2f, -10f, -WorldWidth / 2f ), new Vector3( WorldWidth / 2f, 10f, WorldHeight / 2f ) );
-	public static JumpDefinition NormalJump = new JumpDefinition( "jump", Player.BaseWalkSpeed, Player.JumpHeight, 2 );
-	public static JumpDefinition DiveJump = new JumpDefinition( "dive", Player.DiveStrength / 2f, Player.DiveStrength / 2f, 2 );
+	public static BBox WorldBox => new BBox( new Vector3( -WorldWidth / 2f, -15f, -WorldWidth / 2f ), new Vector3( WorldWidth / 2f, 15f, WorldHeight / 2f ) );
+	public static JumpDefinition LongJump => new JumpDefinition( "longJump", Player.BaseWalkSpeed, Player.JumpHeight * 1.35f, 2, maxPerCell: 1 );
+	public static JumpDefinition HighJump => new JumpDefinition( "highJump", 60f, Player.JumpHeight * 1.35f, 2, maxPerCell: 1 );
 
 	public static async Task GenerateGrid()
 	{
 		var builder = new GridAStar.GridBuilder()
 			.WithBounds( Vector3.Zero, WorldBox, Rotation.Identity )
-			.WithHeightClearance( Player.CollisionHeight)
-			.WithWidthClearance( Player.CollisionWidth)
+			.WithCellSize( Player.CollisionWidth )
+			.WithHeightClearance( Player.CollisionHeight )
+			.WithWidthClearance( Player.CollisionWidth )
 			.WithEdgeNeighbourCount( 2 )
 			.WithStepSize( Player.StepSize )
 			.WithStandableAngle( Player.MaxWalkableAngle )
 			.WithMaxDropHeight( 9999f )
-			.AddJumpDefinition( NormalJump )
-			.AddJumpDefinition( DiveJump )
+			.AddJumpDefinition( LongJump )
+			.AddJumpDefinition( HighJump )
 			.JumpsIgnoreConnections( true );
 
 		await builder.Create( 2, printInfo: true );
