894908734450242f9cbaa654561a2e1e1cbbe0b3
yuberee <59583743+yuberee@users.noreply.github.com>
2023-09-07 23:16:25 +0200

    Implement punch karma and grab karma

--- code/Entities/Player/Bot/Bot.Behaviour.cs
+++ code/Entities/Player/Bot/Bot.Behaviour.cs
@@ -8,13 +8,32 @@ public abstract partial class BotBehaviour
 	public Player Pawn { get; internal set; }
 	public BombSurvivalBot Bot { get; internal set; }
 
+	public Dictionary<Player, float> PunchKarma { get; internal set; } = new Dictionary<Player, float>();
+	public Dictionary<Player, float> GrabKarma { get; internal set; } = new Dictionary<Player, float>();
+
 	public virtual void Compute() { }
 	public virtual void Start() { }
 	public virtual void End() { }
-	public virtual void OnPunch( Player puncher ) { }
-	public virtual void OnKnockout() { }
-	public virtual void OnGrab( Player grabber ) { }
 	public virtual void OnRespawn() { }
+	public virtual void OnKnockout() { }
+	public virtual void OnPunch( Player puncher )
+	{
+		var karmaAdded = 0.1f;
+
+		if ( PunchKarma.ContainsKey( puncher ) )
+			PunchKarma[puncher] = Math.Clamp( PunchKarma[puncher] + karmaAdded, 0f, 1f );
+		else
+			PunchKarma.Add( puncher, karmaAdded );
+	}
+	public virtual void OnGrab( Player grabber ) 
+	{
+		var karmaAdded = 0.1f;
+
+		if ( GrabKarma.ContainsKey( grabber ) )
+			GrabKarma[grabber] = Math.Clamp( GrabKarma[grabber] + karmaAdded, 0f, 1f );
+		else
+			GrabKarma.Add( grabber, karmaAdded );
+	}
 }
 
 public partial class WanderingBehaviour : BotBehaviour
