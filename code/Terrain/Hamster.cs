d5299dd6be885750ed1327b502f08b07a8b463db
yuberee <59583743+yuberee@users.noreply.github.com>
2023-10-01 15:16:52 +0200

    Hamster class that handles everything

--- code/Terrain/Hamster.cs
+++ code/Terrain/Hamster.cs
@@ -3,17 +3,51 @@ using System.Threading.Tasks;
 
 namespace BombSurvival;
 
+public class Hamster
+{
+	public MagicEye LeftEye { get; set; }
+	public MagicEye RightEye { get; set; }
+	public MagicMouth Mouth { get; set; }
+
+	public Hamster( Vector3 position, float scale = 1, string dialogue = "Hello I am tutorial hamser" )
+	{
+		BombSurvival.PlaceHamster( BombSurvival.PointToLocal( position ), scale );
+
+		LeftEye = new MagicEye();
+		LeftEye.Scale = scale;
+		LeftEye.Position = position + ( Vector3.Left * 100f + Vector3.Up * 200f ) * scale;
+		RightEye = new MagicEye();
+		RightEye.Scale = scale;
+		RightEye.Position = position + (Vector3.Right * 100f + Vector3.Up * 200f) * scale;
+		Mouth = new MagicMouth();
+		Mouth.Scale = scale;
+		Mouth.Position = position + ( Vector3.Up * 150f) * scale;
+
+		SetDialogue( dialogue );
+	}
+
+	public void Delete()
+	{
+		LeftEye?.Delete();
+		RightEye?.Delete();
+		Mouth?.Delete();
+	}
+
+	public void SetDialogue( string dialogue ) => Mouth.Dialogue = dialogue;
+}
+
 public partial class BombSurvival
 {
 	public static Texture HamsterFurTexture => Texture.Load( FileSystem.Mounted, $"textures/hamster_fur_sdf.png" );
 	public static Texture HamsterSkinTexture => Texture.Load( FileSystem.Mounted, $"textures/hamster_skin_sdf.png" );
 
-	public async static void PlaceHamster( Vector2 position )
+	public async static void PlaceHamster( Vector2 position, float scale = 1 )
 	{
-		var hamsterFur = new TextureSdf( HamsterFurTexture, 4, 512f )
+		var hamsterFur = new TextureSdf( HamsterFurTexture, 4, 512f * scale )
 			.Transform( position );
-		var hamsterSkin = new TextureSdf( HamsterSkinTexture, 4, 512f )
+		var hamsterSkin = new TextureSdf( HamsterSkinTexture, 4, 512f * scale )
 			.Transform( position );
+
 		await Background?.AddAsync( hamsterFur, GrassBackground );
 		await Background?.AddAsync( hamsterSkin, WoodBackground );
 	}
@@ -22,6 +56,7 @@ public partial class BombSurvival
 	public static void TestHamster()
 	{
 		if ( ConsoleSystem.Caller.Pawn is not Player player ) return;
-		PlaceHamster( PointToLocal( player.Position ) );
+
+		new Hamster( player.Position );
 	}
 }
