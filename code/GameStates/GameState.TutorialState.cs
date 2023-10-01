e849b66e786091d7ec561d671f65a0efb416c173
yuberee <59583743+yuberee@users.noreply.github.com>
2023-10-02 00:18:43 +0200

    Add hamsters in tutorial

--- code/GameStates/GameState.TutorialState.cs
+++ code/GameStates/GameState.TutorialState.cs
@@ -8,6 +8,8 @@ public partial class TutorialState : GameState
 
 		await BombSurvival.GenerateEmptyLevel();
 
+		SpawnHamsters();
+
 		foreach ( var player in Entity.All.OfType<Player>() )
 		{
 			player.Respawn();
@@ -34,5 +36,17 @@ public partial class TutorialState : GameState
 
 		foreach ( var hamster in Hamster.All.ToList() ) // Make a copy so we can safely remove from the original Hamster.All
 			hamster.Delete();
+	}
+
+	public void SpawnHamsters()
+	{
+		new Hamster( new Vector3( -3340f, 0f, 3820f), 0.5f, "Welcome to Bomb Survival! Walk to the right for the tutorial." );
+		new Hamster( new Vector3( -2930f, 0f, 3900f ), 0.5f, "You can hold your SPACE to jump higher!", 300f );
+		new Hamster( new Vector3( -2330f, 0f, 3820f ), 0.5f, "You can dive where you're aiming by presshing SHIFT. Combine it with jumps to get more air!" );
+		new Hamster( new Vector3( -1630f, 0f, 3820f ), 0.5f, "For small spaces, you can walk up to them and you'll crouch automatically. But you'll move slower!" );
+		new Hamster( new Vector3( -840f, 0f, 3820f ), 0.5f, "In that pit you'll find bombs. You need to avoid them if you want to survive! The ones with a dial explode after some time." );
+		new Hamster( new Vector3( 600f, 0f, 3820f ), 0.5f, "Path is blocked? You can grab by aiming and holding RIGHT to move objects out of the way." );
+		new Hamster( new Vector3( 1970f, 0f, 3940f ), 0.5f, "It's dangerous down there. Punch this bot into those mines by aiming and pressing LEFT to open the way." );
+		new Hamster( new Vector3( 3300f, 0f, 3820f ), 0.5f, "HELP ME!!!", 450f );
 	}
 }
--- code/Terrain/Hamster.cs
+++ code/Terrain/Hamster.cs
@@ -10,7 +10,7 @@ public class Hamster
 	public MagicEye RightEye { get; set; }
 	public MagicMouth Mouth { get; set; }
 
-	public Hamster( Vector3 position, float scale = 0.5f, string dialogue = "Hello I am tutorial hamser" )
+	public Hamster( Vector3 position, float scale = 0.5f, string dialogue = "Hello I am tutorial hamser", float range = 200f )
 	{
 		BombSurvival.PlaceHamster( BombSurvival.PointToLocal( position ), scale );
 
@@ -19,15 +19,18 @@ public class Hamster
 		LeftEye.Position = position + (Vector3.Backward * 35f + Vector3.Up * 70f) * scale + Vector3.Left * 25f;
 		LeftEye.Rotation = Rotation.FromYaw( -90f );
 		LeftEye.EyeColor = new Color( 0.2f, 0.4f, 0.6f, 1f );
+		LeftEye.Range = range;
 		RightEye = new MagicEye();
 		RightEye.Scale = scale;
 		RightEye.Position = position + ( Vector3.Forward * 40f + Vector3.Up * 70f ) * scale + Vector3.Left * 25f;
 		RightEye.Rotation = Rotation.FromYaw( -90f );
 		RightEye.EyeColor = new Color( 0.4f, 0.6f, 0.2f, 1f );
+		RightEye.Range = range;
 		Mouth = new MagicMouth();
 		Mouth.Scale = scale;
 		Mouth.Position = position + ( Vector3.Forward * 5f + Vector3.Down * 10f) * scale + Vector3.Left * 25f;
 		Mouth.Rotation = Rotation.FromYaw( -90f );
+		Mouth.Range = range;
 
 		SetDialogue( dialogue );
 
@@ -53,7 +56,7 @@ public partial class BombSurvival
 	public static Texture HamsterFurTexture => Texture.Load( FileSystem.Mounted, $"textures/hamster_fur_sdf.png" );
 	public static Texture HamsterSkinTexture => Texture.Load( FileSystem.Mounted, $"textures/hamster_skin_sdf.png" );
 
-	public async static void PlaceHamster( Vector2 position, float scale = 1 )
+	public async static void PlaceHamster( Vector2 position, float scale = 0.5f )
 	{
 		var hamsterFur = new TextureSdf( HamsterFurTexture, 4, 512f * scale )
 			.Transform( position );
