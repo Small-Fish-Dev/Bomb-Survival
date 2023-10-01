4c9ae8f764f6c6ceb9e28f83a48bc8a998462d14
yuberee <59583743+yuberee@users.noreply.github.com>
2023-10-02 01:17:37 +0200

    Offset helper in knocked motion only if moving down

--- code/Entities/Player/Player.Controller.cs
+++ code/Entities/Player/Player.Controller.cs
@@ -200,7 +200,7 @@ public partial class Player
 		moveHelper.MaxStandableAngle = MaxWalkableAngle;
 
 		moveHelper.Trace = moveHelper.Trace
-			.Size( CollisionBox.Mins + Vector3.Up * 5f, CollisionBox.Maxs )
+			.Size( CollisionBox.Mins + ( Velocity.z <= -10f ? Vector3.Up * 5f : 0f ), CollisionBox.Maxs )
 			.WithoutTags( "puppet", "collider" )
 			.Ignore( this );
 
